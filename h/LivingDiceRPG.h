57cf21d233a814c864c2076c8df2f6d5b2d0fc77
nyhilo <silentclowd@gmail.com>
2016-12-20 18:13:17 -0700

    Revert "Learning how to move files correctly in git -_-"
    This reverts commit 4aa75e79a9087461b9e0b632958ffd87eed68fda.

---
+++ h/LivingDiceRPG.h
@@ -0,0 +1,11 @@
+/*
+Header file for all things for now.
+*/
+
+// Library Includes
+#include <iostream>
+#include <string>
+
+// Class includes
+// Monster.cpp, Weapon.cpp are included by Player.h
+#include "Player.cpp"
